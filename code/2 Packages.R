53d1fc51fc8699e6ebce3a89de6afe4c3b16cf82
unknown <jeff.keller@10162W.i-rsg.com>
2015-07-28 19:14:47 -0400

    Updated required packages list

--- code/2 Packages.R
+++ code/2 Packages.R
@@ -34,5 +34,5 @@ LoadPackages <- function(packages) {
 }
 
 # Load necessary packages (alphabetically, please)
-packages <- c("data.table", "doBy", "ggplot2", "gmodels", "gridExtra", "hexbin", "png", "reshape", "reshape2", "scales", "showtext", "sqldf", "tools")
+packages <- c("data.table", "doBy", "ggplot2", "gmodels", "grid", "gridExtra", "hexbin", "jsonlite", "png", "reshape", "reshape2", "scales", "showtext", "sqldf", "tools")
 LoadPackages(packages)
