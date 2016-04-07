dcd82bf674841e6ea32b3d80ca92177ccc554a6b
Jeffrey Dumont <jeff.dumont@rsginc.com>
2016-04-07 13:58:18 -0400

    Last minute changes before handing over to Justin

--- code/getTimelinessScore.R
+++ code/getTimelinessScore.R
@@ -3,7 +3,7 @@ getTimelinessScore <- function(state,year)
 
   con <- odbcConnect("HPMS")
   
-  score <- sqlQuery(con,paste0("select case when max(submitted_on) > '6/16/",year+1,"' then 0 else 10 end from dbo.[",year,timelinesstable,"] where State_Code = ",state," and Year_Record = ", year))
+  score <- 0#sqlQuery(con,paste0("select case when max(submitted_on) > '6/16/",year+1,"' then 0 else 1 end from dbo.[",year,timelinesstable,"] where State_Code = ",state," and Year_Record = ", year))
   
   odbcClose(con)
   
