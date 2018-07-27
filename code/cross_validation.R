6b7695a4eb42f15415bde91abd99b9de85213aab
Matthew Landis <matt.landis@rsginc.com>
2018-07-27 14:13:22 -0400

    Putting the expand function in its own file

--- code/cross_validation.R
+++ code/cross_validation.R
@@ -1,39 +1,6 @@
 
 
 ###################################################################
-# exapnds data set to 0.01 mile increments for easier joining
-expand = function(data,increment){
-  
-  if (nrow(data) == 0) return(data[, num_sections := vector(mode = 'numeric', length = 0)])
-  
-  data[,begin_point:=round(begin_point+ 0.00001,nchar(1/increment)-1)] # forcing rounding to next highest integer
-  data[,  end_point:=round(end_point  + 0.00001,nchar(1/increment)-1)]
-  
-  # dividing up the num sections so we can recover the 
-  # num sections later
-  data[,num_sections:=1/((end_point-begin_point)/increment)]
-  
-  # how long do we have to make the faux road network
-  range = data[,.(start=min(begin_point),end=max(end_point))]
-  
-  route_network = data.table(end = range[,start]:(range[,end]*(1/increment))/(1/increment))
-  route_network[,start:=end-increment]
-  
-  route_network[,start:=round(start + 0.00001,nchar(1/increment)-1)] # rounding here is necessary to guarantee correct results when looking at inequalities
-  route_network[,  end:=round(end   + 0.00001,nchar(1/increment)-1)]
-  
-  route_network = route_network[start >= 0]
-  
-  data.expanded = data[route_network,
-                       on=.(begin_point <= start, end_point >= end),
-                       allow.cartesian=TRUE]
-
-  data.expanded = data.expanded[!is.na(route_id),]
-  
-  return(data.expanded)
-}
-
-###################################################################
 # summarize the results of the validation
 summarize_validation = function(results){
   
@@ -56,8 +23,6 @@ summarize_validation = function(results){
   
  return(results[["results"]][Applies==TRUE&Passes==TRUE,.(section_total, mileage_total,sections_pass=num_sections/section_total,mileage_pass=mileage/mileage_total)])   
 }
-
-
 
 # -----------------------------------------------------------------------
 
