0c7657cda97e2765e955c66f24e24cf0bbff5db6
Matt Landis <matt.landis@rsginc.com>
2019-08-08 20:43:13 -0400

    fread not working with the enormous HPMS_Review_Sections.  So switching to read_csv_chunked.

--- db_import/hpms_database_import_2018.R
+++ db_import/hpms_database_import_2018.R
@@ -70,35 +70,74 @@ d_dir <- file.path('C:/Users/matt.landis/shared_data/HPMS_download_201908')
 
 # Load 2018 Sections data
 raw_dir <- d_dir
-infile <- file.path(raw_dir, 'HPMS_Sections_w_o_geometry.csv')
-tbl_name <- 'Review_Sections_no_geom'
+# infile <- file.path(raw_dir, 'HPMS_Sections_w_o_geometry.csv')
+infile = file.path(raw_dir, 'HPMS_Review_Sections.csv')
+tbl_name <- 'Review_Sections'
 
 # Write file in chunks of 500k lines
 
-max_rows = 2.8e8
+# max_rows = 117458382  # for no_geom
+
+max_rows = 235380099
 inc_rows = 5e5
 start_rows = seq(0, max_rows, by = inc_rows)
 
 #con <- odbcDriverConnect(connection=local_con)
-con = connect_to_db('burmdlppw01', 'HPMS_2018', intsecurity = TRUE)
 
-for ( start in start_rows ){
-  message('Reading row ', start)
-  tbl = fread(infile, nrows=inc_rows, skip=start, showProgress=TRUE)
-  
-  message('\tWriting')
-  DBI::dbWriteTable(con, name=tbl_name, value=tbl, append=(start==0)) 
+# header = names(fread(infile, nrows=1))
+# test = fread(infile, nrows=50)
+
+test = read_csv(file = infile, n_max = 100)
+
+col_types = cols(
+  Year_Record = col_character(),
+  State_Code = col_integer(),
+  Route_ID = col_character(),
+  Begin_Point = col_double(),
+  End_Point = col_double(),
+  Data_Item = col_character(),
+  Section_Length = col_double(),
+  Value_Numeric = col_double(),
+  Value_Text = col_character(),
+  Value_Date = col_datetime(),
+  Comments = col_character(),
+  Last_Modified_By = col_skip(),
+  Last_Modified_On = col_skip(),
+  Data_Source = col_skip(),
+  StateYearKey = col_integer(),
+  WKT = col_skip(),
+  WKB = col_skip()
+)
+chunk_callback = function(x){
+  dbWriteTable(con, name=tbl_name, value=x, overwrite=FALSE, append=TRUE)
 }
-  
-dbDisconnect(con)
 
 
-# Load 2018 samples data
-infile = file.path(raw_dir, 'HPMS_Review_Sample_Sections.csv')
-tbl_name = 'Review_Sample_Sections'
-tbl = fread(infile, nrows=inc_rows, skip=start, showProgress=TRUE)
-message('\tWriting')
 con = connect_to_db('burmdlppw01', 'HPMS_2018', intsecurity = TRUE)
-dbWriteTable(con, name=tbl_name, value=tbl) 
+read_csv_chunked(infile, callback=chunk_callback, chunk_size=inc_rows,
+                 col_types = col_types, progress=show_progress())
 dbDisconnect(con)
+
+# for ( start in start_rows ){
+#   message('Reading row ', start)
+#   # tbl = fread(infile, nrows=inc_rows, skip=start, showProgress=TRUE, 
+#   #             col.names = header, colClasses='character')
+# 
+#   tbl = read_csv_chunked(file )
+#   message('\tWriting')
+#   DBI::dbWriteTable(con, name=tbl_name, value=tbl,
+#                     overwrite = (start == 0), append=(start > 0)) 
+# }
+#   
+# dbDisconnect(con)
+
+
+# # Load 2018 samples data
+# infile = file.path(raw_dir, 'HPMS_Review_Sample_Sections.csv')
+# tbl_name = 'Review_Sample_Sections'
+# tbl = fread(infile)
+# message('\tWriting')
+# con = connect_to_db('burmdlppw01', 'HPMS_2018', intsecurity = TRUE)
+# dbWriteTable(con, name=tbl_name, value=tbl) 
+# dbDisconnect(con)
 
