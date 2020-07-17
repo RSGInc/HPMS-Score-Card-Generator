a1cb5946c47f1015cc729e77d4647a7269acb08f
Matt Landis <matt.landis@rsginc.com>
2020-07-17 14:18:16 -0400

    Updated review sections data with updated from HPMSresubSevin16

--- db_import/hpms_database_import_2019.R
+++ db_import/hpms_database_import_2019.R
@@ -63,60 +63,56 @@ connect_to_db <- function(server,
   return(con)
 }
 
-
-raw_dir <- file.path('C:/Users/matt.landis/shared_data/HPMS_download_20200626')
-
 # Work ======================================================================
 
 con = connect_to_db('burmdlppw01', 'HPMS', intsecurity = TRUE)
 
-
-# Timeliness table ------------------------------------------------------
-
-tbl = read.socrata(
-  url='https://datahub.transportation.gov/resource/hte9-dvcd.json',
-  # app_token=key
-  email = email,
-  password=password
-)
-
-setDT(tbl)
-str(tbl)
-
-tbl[, year_record := as.integer(year_record)]
-tbl[, state_code := as.integer(state_code)]
-tbl[, submitted_on := mdy_hm(submitted_on)]
-
-tbl_name = 'tt_stage'
-dbWriteTable(con, name=tbl_name, value=tbl, overwrite=TRUE)
-
-# Check
-tt = tbl(con, from=tbl_name)
-glimpse(tt)
-tt %>%
-  count(State_Code) %>%
-  print(n=Inf)
-
-tt %>%
-  count(Year_Record) %>%
-  print(n=Inf)
-
-
-# Sample sections -------------------------------------------------------
-
-url = 'https://datahub.transportation.gov/resource/w6jm-vtp5.csv'
-
-ss = read.socrata(url=url, email=email, password=password)
-setDT(ss)
-str(ss)
-
-tbl_name = 'rss_stage'
-dbWriteTable(con, name=tbl_name, value=ss, overwrite=TRUE)
-
-# Check
-ss = tbl(con, from=tbl_name)
-glimpse(ss)
-
+# # Timeliness table ------------------------------------------------------
+# 
+# tbl = read.socrata(
+#   url='https://datahub.transportation.gov/resource/hte9-dvcd.json',
+#   # app_token=key
+#   email = email,
+#   password=password
+# )
+# 
+# setDT(tbl)
+# str(tbl)
+# 
+# tbl[, year_record := as.integer(year_record)]
+# tbl[, state_code := as.integer(state_code)]
+# tbl[, submitted_on := mdy_hm(submitted_on)]
+# 
+# tbl_name = 'tt_stage'
+# dbWriteTable(con, name=tbl_name, value=tbl, overwrite=TRUE)
+# 
+# # Check
+# tt = tbl(con, from=tbl_name)
+# glimpse(tt)
+# tt %>%
+#   count(State_Code) %>%
+#   print(n=Inf)
+# 
+# tt %>%
+#   count(Year_Record) %>%
+#   print(n=Inf)
+# 
+# 
+# # Sample sections -------------------------------------------------------
+# 
+# url = 'https://datahub.transportation.gov/resource/w6jm-vtp5.csv'
+# 
+# ss = read.socrata(url=url, email=email, password=password)
+# setDT(ss)
+# str(ss)
+# 
+# tbl_name = 'rss_stage'
+# dbWriteTable(con, name=tbl_name, value=ss, overwrite=TRUE)
+# 
+# # Check
+# ss = tbl(con, from=tbl_name)
+# glimpse(ss)
+# 
 
 # Load Sections data ---------------------------------------------------
 
@@ -139,19 +135,23 @@ col_type_chk = c(
 
 coltype_chk_dt = data.table(field = names(col_type_chk), chk = col_type_chk)
 
-urls = c(
-  new_england = 'https://datahub.transportation.gov/resource/yed4-dz8a.csv',
-  heartland = 'https://datahub.transportation.gov/resource/pu8w-cqik.csv',
-  gulf = 'https://datahub.transportation.gov/resource/rf6n-m9pz.csv',
-  appalachia = 'https://datahub.transportation.gov/resource/j6bh-426b.csv',
-  mid_atlantic = 'https://datahub.transportation.gov/resource/f674-dyn6.csv',
-  lakes = 'https://datahub.transportation.gov/resource/xq7x-rndy.csv',
-  desert = 'https://datahub.transportation.gov/resource/gwx5-yka7.csv',
-  islands = 'https://datahub.transportation.gov/resource/imwx-p856.csv',
-  west = 'https://datahub.transportation.gov/resource/pdrm-udaf.csv',
-  badlands = 'https://datahub.transportation.gov/resource/gve6-cr9a.csv'
-)
+# urls = c(
+#   new_england = 'https://datahub.transportation.gov/resource/yed4-dz8a.csv',
+#   heartland = 'https://datahub.transportation.gov/resource/pu8w-cqik.csv',
+#   gulf = 'https://datahub.transportation.gov/resource/rf6n-m9pz.csv',
+#   appalachia = 'https://datahub.transportation.gov/resource/j6bh-426b.csv',
+#   mid_atlantic = 'https://datahub.transportation.gov/resource/f674-dyn6.csv',
+#   lakes = 'https://datahub.transportation.gov/resource/xq7x-rndy.csv',
+#   desert = 'https://datahub.transportation.gov/resource/gwx5-yka7.csv',
+#   islands = 'https://datahub.transportation.gov/resource/imwx-p856.csv',
+#   west = 'https://datahub.transportation.gov/resource/pdrm-udaf.csv',
+#   badlands = 'https://datahub.transportation.gov/resource/gve6-cr9a.csv'
+# )
 
+# Set url to resubmissions
+urls = c(
+  resub = 'https://datahub.transportation.gov/resource/yq8j-yqf2.csv'
+)
 
 for ( i in seq_along(urls) ){
   message('Downloading ', names(urls)[i])
@@ -178,11 +178,12 @@ for ( i in seq_along(urls) ){
   
 }
 # Check
+
 dt = tbl(con, from=tbl_name)
 glimpse(dt)
 
 counts_local = dt %>%
-  count(state_code) %>%
+  count(state_code, year_record) %>%
   collect() %>%
   rename(n_local = n)
 
@@ -193,121 +194,120 @@ counts_remote_list = list()
 for ( i in seq_along(urls) ){
   message('Fetching counts for ', names(urls)[i])
   url = URLencode(paste0(urls[i], '?$select=state_code,data_item'))
-  url = paste0(urls[i], '?$query=SELECT state_code, count(state_code) GROUP BY state_code ORDER BY state_code')
+  url = paste0(urls[i], '?$query=SELECT state_code,year_record,count(state_code) GROUP BY state_code, year_record ORDER BY state_code, year_record')
   counts_remote_list[[names(urls)[i] ]] = read.socrata2(url, email=email, password=password)
 }
 
 counts_remote = rbindlist(counts_remote_list, idcol = 'region')
 setnames(counts_remote, 'count_state_code', 'n_remote')
 
-counts_check = merge(counts_remote, counts_local, by = 'state_code', all=TRUE)
+counts_check = merge(counts_remote, counts_local,
+                     by = c('state_code', 'year_record'), all=TRUE)
 
 stopifnot(counts_check[n_local != n_remote, .N] == 0)
 
 
 # Move data from stage table to production table =======================
 
-con = connect_to_db('burmdlppw01', 'HPMS', intsecurity = TRUE)
-current_yr = 2019
+# con = connect_to_db('burmdlppw01', 'HPMS', intsecurity = TRUE)
 
-
-# Timelinesstable --------------------------------------------------------------
-
-# Check for existing current year
-
-tt = tbl(con, from='Timelinesstable')
-
-yr_count = tt %>%
-  filter(year_record == current_yr) %>%
-  count() %>%
-  pull(n)
-
-if ( yr_count > 0 ){
-  dbExecute(con, paste0('DELETE FROM Timelinesstable WHERE year_record = ',
-                        current_yr))
-}
-
-yr_count = tt %>%
-  filter(year_record == current_yr) %>%
-  count() %>%
-  pull(n)
-
-stopifnot(yr_count== 0)
-
-old_fields = dbListFields(con, 'Timelinesstable')
-new_fields = dbListFields(con, 'tt_stage')
-
-setdiff(new_fields, tolower(old_fields))
-setdiff(tolower(old_fields), new_fields)
-
-# Copy current year into full table.
-sql = paste0('insert into Timelinesstable(', paste(new_fields, collapse=', '),
-             ') select ', paste(new_fields, collapse=', '), ' from tt_stage')
-dbExecute(con, sql)
-          
-
-stopifnot(
-  con %>%
-    tbl('Timelinesstable') %>%
-    filter(year_record == current_yr) %>%
-    count() %>%
-    pull(n) ==
-    con %>%
-    tbl('tt_stage') %>%
-    count() %>%
-    pull(n)
-)
-
-
-
-# Sample Sections --------------------------------------------------------------
-
-
-ss = tbl(con, from='Review_Sample_Sections')
-
-yr_count = ss %>%
-  filter(year_record == current_yr) %>%
-  count() %>%
-  pull(n)
-
-if ( yr_count > 0 ){
-  dbExecute(con, paste0('DELETE FROM Review_Sample_Sections WHERE year_record = ',
-                             current_yr))
-}
-
-yr_count = ss %>%
-  filter(year_record == 2019) %>%
-  count() %>%
-  pull(n)
-
-stopifnot(yr_count== 0)
-
-old_fields = dbListFields(con, 'Review_Sample_Sections')
-new_fields = dbListFields(con, 'rss_stage')
-
-setdiff(new_fields, tolower(old_fields))
-
-# Create StateYearKey
-dbExecute(con, 'alter table rss_stage add StateYearKey as (state_code * 100 + year_record % 1000)')
-
-# Copy current year into full table.
-dbExecute(con,
-          'insert into Review_Sample_Sections(year_record, state_code, route_id, begin_point, end_point, section_length, sample_id, expansion_factor, stateyearkey) 
-           select year_record, state_code, route_id, begin_point, end_point,
-           section_length, sample_id, expansion_factor, stateyearkey from rss_stage')
-
-stopifnot(
-  con %>%
-      tbl('Review_Sample_Sections') %>%
-      filter(year_record == current_yr) %>%
-      count() %>%
-      pull(n) ==
-    con %>%
-      tbl('rss_stage') %>%
-      count() %>%
-      pull(n)
-)
-
+# # Timelinesstable --------------------------------------------------------------
+# 
+# # Check for existing current year
+# 
+# tt = tbl(con, from='Timelinesstable')
+# 
+# yr_count = tt %>%
+#   filter(year_record == current_yr) %>%
+#   count() %>%
+#   pull(n)
+# 
+# if ( yr_count > 0 ){
+#   dbExecute(con, paste0('DELETE FROM Timelinesstable WHERE year_record = ',
+#                         current_yr))
+# }
+# 
+# yr_count = tt %>%
+#   filter(year_record == current_yr) %>%
+#   count() %>%
+#   pull(n)
+# 
+# stopifnot(yr_count== 0)
+# 
+# old_fields = dbListFields(con, 'Timelinesstable')
+# new_fields = dbListFields(con, 'tt_stage')
+# 
+# setdiff(new_fields, tolower(old_fields))
+# setdiff(tolower(old_fields), new_fields)
+# 
+# # Copy current year into full table.
+# sql = paste0('insert into Timelinesstable(', paste(new_fields, collapse=', '),
+#              ') select ', paste(new_fields, collapse=', '), ' from tt_stage')
+# dbExecute(con, sql)
+#           
+# 
+# stopifnot(
+#   con %>%
+#     tbl('Timelinesstable') %>%
+#     filter(year_record == current_yr) %>%
+#     count() %>%
+#     pull(n) ==
+#     con %>%
+#     tbl('tt_stage') %>%
+#     count() %>%
+#     pull(n)
+# )
+# 
+# 
+# 
+# # Sample Sections --------------------------------------------------------------
+# 
+# 
+# ss = tbl(con, from='Review_Sample_Sections')
+# 
+# yr_count = ss %>%
+#   filter(year_record == current_yr) %>%
+#   count() %>%
+#   pull(n)
+# 
+# if ( yr_count > 0 ){
+#   dbExecute(con, paste0('DELETE FROM Review_Sample_Sections WHERE year_record = ',
+#                              current_yr))
+# }
+# 
+# yr_count = ss %>%
+#   filter(year_record == 2019) %>%
+#   count() %>%
+#   pull(n)
+# 
+# stopifnot(yr_count== 0)
+# 
+# old_fields = dbListFields(con, 'Review_Sample_Sections')
+# new_fields = dbListFields(con, 'rss_stage')
+# 
+# setdiff(new_fields, tolower(old_fields))
+# 
+# # Create StateYearKey
+# dbExecute(con, 'alter table rss_stage add StateYearKey as (state_code * 100 + year_record % 1000)')
+# 
+# # Copy current year into full table.
+# dbExecute(con,
+#           'insert into Review_Sample_Sections(year_record, state_code, route_id, begin_point, end_point, section_length, sample_id, expansion_factor, stateyearkey) 
+#            select year_record, state_code, route_id, begin_point, end_point,
+#            section_length, sample_id, expansion_factor, stateyearkey from rss_stage')
+# 
+# stopifnot(
+#   con %>%
+#       tbl('Review_Sample_Sections') %>%
+#       filter(year_record == current_yr) %>%
+#       count() %>%
+#       pull(n) ==
+#     con %>%
+#       tbl('rss_stage') %>%
+#       count() %>%
+#       pull(n)
+# )
+# 
 
 
 # Review Sections --------------------------------------------------------------
@@ -315,25 +315,16 @@ stopifnot(
 prod_table = 'Review_Sections'
 stage_table = 'rs_stage'
 
+years = unique(counts_local$year_record)
+states = unique(counts_local$state_code)
+
 prod = tbl(con, from=prod_table)
 
-yr_count = prod %>%
-  filter(year_record == current_yr) %>%
-  count() %>%
-  pull(n)
-
-if ( yr_count > 0 ){
-  sql = paste0('DELETE FROM ', prod_table, ' WHERE year_record = ',
-               current_yr)
-  dbExecute(con, sql)
-}
-
-yr_count = prod %>%
-  filter(year_record == current_yr) %>%
-  count() %>%
-  pull(n)
-
-stopifnot(yr_count== 0)
+sql = paste0(
+    'DELETE FROM ', prod_table,
+    ' WHERE state_code in (', paste(states, collapse=', '), ')',
+    ' AND year_record in (', paste(years, collapse=', '), ')')
+dbExecute(con, sql)
 
 old_fields = dbListFields(con, prod_table)
 new_fields = dbListFields(con, stage_table)
@@ -354,17 +345,16 @@ sql = paste0('insert into ', prod_table, '(', paste(new_fields, collapse=', '),
              ') select ', paste(new_fields, collapse=', '), ' from ', stage_table)
 dbExecute(con, sql)
 
+# Get counts in production table
+counts_prod = prod %>%
+  filter(year_record %in% years,
+         state_code %in% states) %>%
+  count(state_code, year_record) %>%
+  collect() %>%
+  rename(n_prod = n)
 
-stopifnot(
-  con %>%
-    tbl(prod_table) %>%
-    filter(year_record == current_yr) %>%
-    count() %>%
-    pull(n) ==
-    con %>%
-    tbl(stage_table) %>%
-    count() %>%
-    pull(n)
-)
+counts_check = merge(counts_check, counts_prod, by = c('state_code', 'year_record'))
+
+stopifnot(counts_check[n_local != n_prod, .N] == 0)
 
 dbDisconnect(con)
