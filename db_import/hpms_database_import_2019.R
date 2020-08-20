d7a9894ec8ddbab854824e8badec7e85f1129fd8
Matt Landis <matt.landis@rsginc.com>
2020-08-20 16:20:50 -0400

    Reorganized download script into functions to make resubmission easier to handle.

--- db_import/hpms_database_import_2019.R
+++ db_import/hpms_database_import_2019.R
@@ -16,6 +16,124 @@ library('RSocrata')
 source('db_import/socrata_functions.R')
 source('code/connect_to_db.R')
 
+# Functions =================================================================
+
+download_socrata = function(url, con, stage_table){
+  
+  # For checking column types
+  col_type_chk = c(
+    year_record = 'integer',
+    state_code = 'integer',
+    route_id = 'character',
+    begin_point = 'numeric',
+    end_point = 'numeric',
+    data_item = 'character',
+    value_numeric = 'numeric',
+    value_text = 'character',
+    value_date = 'POSIXct',
+    natroute_id = 'character'
+  )
+  
+  coltype_chk_dt = data.table(field = names(col_type_chk), chk = col_type_chk)
+  
+  
+  query = paste0(url, '?$query=SELECT state_code,year_record,count(state_code) GROUP BY state_code, year_record ORDER BY state_code, year_record')
+  counts_remote = read.socrata2(query, email=email, password=password)
+  setnames(counts_remote, 'count_state_code', 'n_remote')
+  
+  message('Downloading ', sum(counts_remote[n_remote]), ' rows from ', names(url))
+  
+  dt = read.socrata(url=url, email=email, password=password)
+  setDT(dt)
+  
+  if ( names(urls)[i] == 'mid_atlantic' ){
+    dt[, value_date := ymd_hms(value_date)]
+  }
+  
+  col_type_obs = sapply(dt, function(x) class(x)[1])
+  coltype_obs_dt = data.table(field = names(col_type_obs), obs = col_type_obs)
+  
+  coltype_dt = merge(coltype_chk_dt, coltype_obs_dt, by='field')
+  
+  if ( coltype_dt[chk != obs, .N] > 0 ){
+    message('col type mismatch - writing to file')
+    filename = file.path(raw_dir, paste0(names(urls)[i], '.rds'))
+    saveRDS(dt, filename)
+  } else {
+    message('writing to database')
+    dbWriteTable(con, stage_table, dt, append=TRUE)
+  }
+  
+  # Check
+  
+  dt = tbl(con, from=stage_table)
+  # glimpse(dt)
+  
+  # Check that we have the right number of rows for each state.
+  
+  counts_local = stage %>%
+    count(state_code, year_record) %>%
+    collect() %>%
+    rename(n_local = n)
+  
+  counts_check = merge(counts_remote, counts_local,
+                       by = c('state_code', 'year_record'), all=TRUE)
+  setDT(counts_check)
+  stopifnot(counts_check[n_local != n_remote, .N] == 0)
+  
+  return(counts_local)
+}
+
+copy_rows = function(con, prod_table, stage_table, counts_local){
+  # Move data from stage to production -----------------------------------------
+  
+  years = unique(counts_local$year_record)
+  states = unique(counts_local$state_code)
+  
+  prod = tbl(con, from=prod_table)
+  
+  sql = paste0(
+    'DELETE FROM ', prod_table,
+    ' WHERE state_code in (', paste(states, collapse=', '), ')',
+    ' AND year_record in (', paste(years, collapse=', '), ')')
+  dbExecute(con, sql)
+  
+  old_fields = dbListFields(con, prod_table)
+  new_fields = dbListFields(con, stage_table)
+  
+  setdiff(tolower(new_fields), tolower(old_fields))
+  new_fields = new_fields[!new_fields %in% 'natroute_id']
+  
+  setdiff(tolower(old_fields), tolower(new_fields))
+  
+  # Create StateYearKey
+  if ( !'StateYearKey' %in% new_fields ){
+    sql = paste0('alter table ', stage_table,
+                 ' add StateYearKey as (state_code * 100 + year_record % 1000)')
+    dbExecute(con, sql)
+    new_fields = c(new_fields, 'StateYearKey')
+  }
+  
+  # Copy rows into production table.
+  sql = paste0('insert into ', prod_table, '(', paste(new_fields, collapse=', '),
+               ') select ', paste(new_fields, collapse=', '), ' from ', stage_table)
+  dbExecute(con, sql)
+  
+  # Get counts in production table
+  counts_prod = prod %>%
+    filter(year_record %in% years,
+           state_code %in% states) %>%
+    count(state_code, year_record) %>%
+    collect() %>%
+    rename(n_prod = n)
+  
+  counts_check = merge(counts_local, counts_prod, by = c('state_code', 'year_record'))
+  setDT(counts_check)
+  
+  stopifnot(counts_check[n_local != n_prod, .N] == 0)
+}
+
+
 email = 'matt.landis@rsginc.com'
 password = 'tX522b7Dz4xS'
 
@@ -26,6 +144,93 @@ password = 'tX522b7Dz4xS'
 # Work ======================================================================
 
 con = connect_to_db('burmdlppw01', 'HPMS', intsecurity = TRUE)
+
+
+
+
+# Sample sections -------------------------------------------------------
+
+prod_table = 'Review_Sample_Sections'
+stage_table = 'rss_stage'
+
+# Original data
+# url = 'https://datahub.transportation.gov/resource/w6jm-vtp5.json'
+
+# Resubmission
+# url = 'https://datahub.transportation.gov/resource/b37r-yiaq.json'
+
+# Resubmissions 20 Aug 2020
+
+sample_urls = c(
+  MN = 'https://datahub.transportation.gov/Roadways-and-Bridges/MinnesotaSample2019/83bz-5v5q',
+  MD = 'https://datahub.transportation.gov/Roadways-and-Bridges/MarylandSample2019/kmgx-n6xj',
+  NM = 'https://datahub.transportation.gov/dataset/HPMSNewMexicoSample2019/x6js-ep8c',
+  SC = 'https://datahub.transportation.gov/dataset/HPMSSouthCarolinaSample2019/w5m2-vgxi',
+  AZ = 'https://datahub.transportation.gov/dataset/HPMSArizonaSample2019/rv3n-ux6r',
+  TN = 'https://datahub.transportation.gov/dataset/HPMSTennesseeSample2019/yr8t-q9gm',
+  PR = 'https://datahub.transportation.gov/dataset/HPMSPuertoRicoSample2019/2smy-8bva',
+  ND = 'https://datahub.transportation.gov/dataset/HPMSNorthDakotaSample2018/6ss7-4v5q',
+  LA = 'https://datahub.transportation.gov/dataset/HPMSLouisianaSample2019/c274-he8g',
+  HI = 'https://datahub.transportation.gov/dataset/HPMSHawaiiSample2019/fhtn-3aua',
+  CA = 'https://datahub.transportation.gov/dataset/HPMSCaliforniaSample2019/wev8-4s59'  
+)
+
+
+for ( url in sample_urls ){
+  counts_local = download_socrata(url, con, stage_table)
+  copy_rows(con, prod_table, stage_table, counts_local)
+}
+
+
+
+# Sections data ---------------------------------------------------
+
+prod_table = 'Review_Sections'
+stage_table = 'rs_stage'
+
+# urls = c(
+#   new_england = 'https://datahub.transportation.gov/resource/yed4-dz8a.json',
+#   heartland = 'https://datahub.transportation.gov/resource/pu8w-cqik.json',
+#   gulf = 'https://datahub.transportation.gov/resource/rf6n-m9pz.json',
+#   appalachia = 'https://datahub.transportation.gov/resource/j6bh-426b.json',
+#   mid_atlantic = 'https://datahub.transportation.gov/resource/f674-dyn6.json',
+#   lakes = 'https://datahub.transportation.gov/resource/xq7x-rndy.json',
+#   desert = 'https://datahub.transportation.gov/resource/gwx5-yka7.json',
+#   islands = 'https://datahub.transportation.gov/resource/imwx-p856.json',
+#   west = 'https://datahub.transportation.gov/resource/pdrm-udaf.json',
+#   badlands = 'https://datahub.transportation.gov/resource/gve6-cr9a.json'
+# )
+
+# Set url to resubmissions
+# urls = c(
+#   resub = 'https://datahub.transportation.gov/resource/yq8j-yqf2.json'
+# )
+
+# Resubmissions on 20 Aug 2020
+
+sections_urls = c(
+  MN = 'https://datahub.transportation.gov/Roadways-and-Bridges/MinnesotaSections2019/3uf9-qbi2',
+  MD = 'https://datahub.transportation.gov/Roadways-and-Bridges/MarylandSections2019/ddzr-fkg3',
+  NM = 'https://datahub.transportation.gov/dataset/HPMSNewMexicoSections2019/9tf9-8qq4',
+  SC = 'https://datahub.transportation.gov/dataset/HPMSSouthCarolinaSections2019/axzv-af4r',
+  AZ = 'https://datahub.transportation.gov/dataset/HPMSArizonaSections2019/undi-6wdr',
+  TN = 'https://datahub.transportation.gov/dataset/HPMSTennesseeSections2019/djic-ekmg',
+  PR = 'https://datahub.transportation.gov/dataset/HPMSPuertoRicoSections2019/yrrm-p8fu',
+  ND = 'https://datahub.transportation.gov/dataset/HPMSNorthDakotaSections2019/7v4t-66uv',
+  LA = 'https://datahub.transportation.gov/dataset/HPMSLouisianaSections2019/6h5i-ej6p',
+  HI = 'https://datahub.transportation.gov/dataset/HPMSHawaiiSections2019/tuf3-6qxe',
+  CA = 'https://datahub.transportation.gov/dataset/HPMSCaliforniaSections2019/mydz-ydth'
+)
+
+for ( i in seq_along(urls) ){
+  
+  counts_local = download_socrata(url, con, stage_table)
+  copy_rows(con, prod_table, stage_table, counts_local)
+  
+}
+
+
+dbDisconnect(con)
 
 # # Timeliness table ------------------------------------------------------
 # 
@@ -43,11 +248,11 @@ con = connect_to_db('burmdlppw01', 'HPMS', intsecurity = TRUE)
 # tbl[, state_code := as.integer(state_code)]
 # tbl[, submitted_on := mdy_hm(submitted_on)]
 # 
-# tbl_name = 'tt_stage'
-# dbWriteTable(con, name=tbl_name, value=tbl, overwrite=TRUE)
+# stage_table = 'tt_stage'
+# dbWriteTable(con, name=stage_table, value=tbl, overwrite=TRUE)
 # 
 # # Check
-# tt = tbl(con, from=tbl_name)
+# tt = tbl(con, from=stage_table)
 # glimpse(tt)
 # tt %>%
 #   count(State_Code) %>%
@@ -105,237 +310,3 @@ con = connect_to_db('burmdlppw01', 'HPMS', intsecurity = TRUE)
 # )
 #
 # 
-# Sample sections -------------------------------------------------------
-
-# Original data
-# url = 'https://datahub.transportation.gov/resource/w6jm-vtp5.json'
-
-# Resubmission
-url = 'https://datahub.transportation.gov/resource/b37r-yiaq.json'
-
-ss = read.socrata(url=url, email=email, password=password)
-setDT(ss)
-str(ss)
-
-tbl_name = 'rss_stage'
-dbWriteTable(con, name=tbl_name, value=ss, overwrite=TRUE)
-
-# Check
-stage = tbl(con, from=tbl_name)
-glimpse(stage)
-
-counts_local = stage %>%
-  count(state_code, year_record) %>%
-  collect() %>%
-  rename(n_local = n)
-
-
-# Check that we have the right number of rows for each state.
-
-query = paste0(url, '?$query=SELECT state_code,year_record,count(state_code) GROUP BY state_code, year_record ORDER BY state_code, year_record')
-counts_remote = read.socrata2(query, email=email, password=password)
-setnames(counts_remote, 'count_state_code', 'n_remote')
-counts_check = merge(counts_remote, counts_local,
-                     by = c('state_code', 'year_record'), all=TRUE)
-setDT(counts_check)
-stopifnot(counts_check[n_local != n_remote, .N] == 0)
-
-
-# Move data from stage to production -----------------------------------------
-
-prod_table = 'Review_Sample_Sections'
-stage_table = 'rss_stage'
-
-years = unique(counts_local$year_record)
-states = unique(counts_local$state_code)
-
-prod = tbl(con, from=prod_table)
-n_prod = prod %>% 
-  filter(State_Code %in% states, 
-         Year_Record %in% years) %>%
-  count(State_Code, Year_Record) %>%
-  collect()
-
-sql = paste0(
-  'DELETE FROM ', prod_table,
-  ' WHERE state_code in (', paste(states, collapse=', '), ')',
-  ' AND year_record in (', paste(years, collapse=', '), ')')
-dbExecute(con, sql)
-
-old_fields = dbListFields(con, prod_table)
-new_fields = dbListFields(con, stage_table)
-
-setdiff(tolower(new_fields), tolower(old_fields))
-new_fields = new_fields[!new_fields %in% 'natroute_id']
-
-setdiff(tolower(old_fields), tolower(new_fields))
-
-
-# Create StateYearKey
-dbExecute(con, 'alter table rss_stage add StateYearKey as (state_code * 100 + year_record % 1000)')
-
-# Copy current year into full table.
-dbExecute(con,
-          'insert into Review_Sample_Sections(year_record, state_code, route_id, begin_point, end_point, section_length, sample_id, expansion_factor, stateyearkey)
-           select year_record, state_code, route_id, begin_point, end_point,
-           section_length, sample_id, expansion_factor, stateyearkey from rss_stage')
-
-counts_prod = prod %>% 
-  filter(state_code %in% states,
-         year_record %in% years) %>%
-  count(state_code, year_record) %>%
-  collect()
-setnames(counts_prod, 'n', 'n_prod')
-
-counts_check = merge(counts_check, counts_prod,
-                     by = c('state_code', 'year_record'), all=TRUE)
-
-stopifnot(all(counts_check[, n_prod] == counts_check[, n_local]))
-
-
-# Load Sections data ---------------------------------------------------
-
-tbl_name <- 'rs_stage'
-
-# For checking column types
-col_type_chk = c(
-  year_record = 'integer',
-  state_code = 'integer',
-  route_id = 'character',
-  begin_point = 'numeric',
-  end_point = 'numeric',
-  data_item = 'character',
-  value_numeric = 'numeric',
-  value_text = 'character',
-  value_date = 'POSIXct',
-  natroute_id = 'character'
-)
-
-coltype_chk_dt = data.table(field = names(col_type_chk), chk = col_type_chk)
-
-# urls = c(
-#   new_england = 'https://datahub.transportation.gov/resource/yed4-dz8a.json',
-#   heartland = 'https://datahub.transportation.gov/resource/pu8w-cqik.json',
-#   gulf = 'https://datahub.transportation.gov/resource/rf6n-m9pz.json',
-#   appalachia = 'https://datahub.transportation.gov/resource/j6bh-426b.json',
-#   mid_atlantic = 'https://datahub.transportation.gov/resource/f674-dyn6.json',
-#   lakes = 'https://datahub.transportation.gov/resource/xq7x-rndy.json',
-#   desert = 'https://datahub.transportation.gov/resource/gwx5-yka7.json',
-#   islands = 'https://datahub.transportation.gov/resource/imwx-p856.json',
-#   west = 'https://datahub.transportation.gov/resource/pdrm-udaf.json',
-#   badlands = 'https://datahub.transportation.gov/resource/gve6-cr9a.json'
-# )
-
-# Set url to resubmissions
-urls = c(
-  resub = 'https://datahub.transportation.gov/resource/yq8j-yqf2.json'
-)
-
-for ( i in seq_along(urls) ){
-  message('Downloading ', names(urls)[i])
-  dt = read.socrata(url=urls[i], email=email, password=password)
-  setDT(dt)
-  
-  if ( names(urls)[i] == 'mid_atlantic' ){
-    dt[, value_date := ymd_hms(value_date)]
-  }
-  
-  col_type_obs = sapply(dt, function(x) class(x)[1])
-  coltype_obs_dt = data.table(field = names(col_type_obs), obs = col_type_obs)
-  
-  coltype_dt = merge(coltype_chk_dt, coltype_obs_dt, by='field')
-  
-  if ( coltype_dt[chk != obs, .N] > 0 ){
-    message('col type mismatch - writing to file')
-    filename = file.path(raw_dir, paste0(names(urls)[i], '.rds'))
-    saveRDS(dt, filename)
-  } else {
-    message('writing to database')
-    dbWriteTable(con, tbl_name, dt, append=TRUE)
-  }
-  
-}
-# Check
-
-dt = tbl(con, from=tbl_name)
-glimpse(dt)
-
-counts_local = dt %>%
-  count(state_code, year_record) %>%
-  collect() %>%
-  rename(n_local = n)
-
-
-# Check that we have the right number of rows for each state.
-
-counts_remote_list = list()
-for ( i in seq_along(urls) ){
-  message('Fetching counts for ', names(urls)[i])
-  url = paste0(urls[i], '?$query=SELECT state_code,year_record,count(state_code) GROUP BY state_code, year_record ORDER BY state_code, year_record')
-  counts_remote_list[[names(urls)[i] ]] = read.socrata2(url, email=email, password=password)
-}
-
-counts_remote = rbindlist(counts_remote_list, idcol = 'region')
-setnames(counts_remote, 'count_state_code', 'n_remote')
-
-counts_check = merge(counts_remote, counts_local,
-                     by = c('state_code', 'year_record'), all=TRUE)
-
-stopifnot(counts_check[n_local != n_remote, .N] == 0)
-
-
-
-# Move data from stage to production ------------------------------------------
- 
-
-# Review Sections --------------------------------------------------------------
-
-prod_table = 'Review_Sections'
-stage_table = 'rs_stage'
-
-years = unique(counts_local$year_record)
-states = unique(counts_local$state_code)
-
-prod = tbl(con, from=prod_table)
-
-sql = paste0(
-    'DELETE FROM ', prod_table,
-    ' WHERE state_code in (', paste(states, collapse=', '), ')',
-    ' AND year_record in (', paste(years, collapse=', '), ')')
-dbExecute(con, sql)
-
-old_fields = dbListFields(con, prod_table)
-new_fields = dbListFields(con, stage_table)
-
-setdiff(tolower(new_fields), tolower(old_fields))
-new_fields = new_fields[!new_fields %in% 'natroute_id']
-
-setdiff(tolower(old_fields), tolower(new_fields))
-
-# Create StateYearKey
-if ( !'StateYearKey' %in% new_fields ){
-  sql = paste0('alter table ', stage_table,
-               ' add StateYearKey as (state_code * 100 + year_record % 1000)')
-  dbExecute(con, sql)
-  new_fields = c(new_fields, 'StateYearKey')
-}
-
-# Copy current year into full table.
-sql = paste0('insert into ', prod_table, '(', paste(new_fields, collapse=', '),
-             ') select ', paste(new_fields, collapse=', '), ' from ', stage_table)
-dbExecute(con, sql)
-
-# Get counts in production table
-counts_prod = prod %>%
-  filter(year_record %in% years,
-         state_code %in% states) %>%
-  count(state_code, year_record) %>%
-  collect() %>%
-  rename(n_prod = n)
-
-counts_check = merge(counts_local, counts_prod, by = c('state_code', 'year_record'))
-setDT(counts_check)
-
-stopifnot(counts_check[n_local != n_prod, .N] == 0)
-
-dbDisconnect(con)
