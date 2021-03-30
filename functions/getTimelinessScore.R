230f7a0e517afc6e68e9a5292c12e75ff3be192c
Matt Landis <matt.landis@rsginc.com>
2021-03-30 14:57:38 -0400

    Adding changes to getTimelineessScore.R

--- functions/getTimelinessScore.R
+++ functions/getTimelinessScore.R
@@ -15,14 +15,28 @@ getTimelinessScore <- function(state, year,
                                submission_deadline = paste0(year + 1, '-06-16'))
 {
 
-  sub_date = format(ymd(submission_deadline), '%m/%d/%Y 23:59:00')
-  con <- odbcConnect("HPMS")
-  
-  score <- sqlQuery(con,
-                    paste0("select case when max(submitted_on) > '", sub_date,
-                           "' then 0 else 1 end from dbo.[",timelinesstable,"] where State_Code = ",state," and Year_Record = ", year))
-  
-  odbcClose(con)
+  if ( exists('gTimeliness') ){
+    
+    deadline = ymd_hms(paste0(submission_deadline, ' 23:59:59'))
+    
+    sub_date = gTimeliness[State_Code == state & Year_Record == year, max(Submitted_On)]
+    
+    score = 1 * (sub_date < deadline)
+    
+  } else {
+    
+    deadline = format(ymd(submission_deadline), '%m/%d/%Y 23:59:59')
+    
+    con <- odbcConnect("HPMS")
+    
+    score <- sqlQuery(con,
+      paste0("select case when max(submitted_on) > '", deadline,
+        "' then 0 else 1 end from dbo.[",timelinesstable,"] where State_Code = ",state," and Year_Record = ", year))
+    
+    score = score[1, 1]
+    
+    odbcClose(con)
+  }
   
   return(score)
 }
