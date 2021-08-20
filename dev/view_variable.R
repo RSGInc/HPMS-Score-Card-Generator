d0ae7bc72db032097dba4959cd667d4334bf285f
Matt Landis <matt.landis@rsginc.com>
2021-08-20 10:24:22 -0400

    Updated jig scripts for running and debugging

--- dev/view_variable.R
+++ dev/view_variable.R
@@ -5,9 +5,10 @@
 # The minimum code necessary to view a particular variable
 # for developing graphic displays
 
-# Load Code -------------------------------------------------------------------
-invisible(sapply(X = list.files(path = "code", pattern = "*.R$",
-                                full.names = TRUE)[-1], FUN = source))
+# Load Code
+codefiles = c(Sys.glob('app/*.R'), Sys.glob('functions/*.R'))
+invisible(sapply(X =codefiles , FUN = source))
+
 
 view_variable = function(data.list, variable = NULL, var.label = NULL){
   
@@ -33,62 +34,19 @@ view_variable = function(data.list, variable = NULL, var.label = NULL){
                       ramps=FALSE)
 }
 
-year_selection=2019
-year_compare = 2018
 
-debug(densityPlot)
-state = 'CA'
+# Set parameters
+
+year_selection=2020
+year_compare = 2019
+state = 'TX'
+
+
+# Load data
+
 data.list = getStateDataSets(state, year_selection, year_compare)
-view_variable(data.list, var.label='Through Lanes')
+
+debug(create_page_summary)
+view_variable(data.list, var.label='Cracking Percent')
 
 
-state = 'NM'
-data.list = getStateDataSets(state, year_selection, year_compare)
-view_variable(data.list, var.label='Through Lanes')
-
-state = 'DC'
-data.list = getStateDataSets(state, year_selection, year_compare)
-view_variable(data.list, var.label='Faulting')
-view_variable(data.list, variable='CAPACITY')
-
-state = 'MT'
-data.list = getStateDataSets(state, year_selection, year_compare)
-view_variable(data.list, var.label = 'Median Width')
-view_variable(data.list, var.label = 'Cracking Percent')
-
-state = 'PA'  
-data.list <- getStateDataSets(state, year_selection, year_compare)
-
-view_variable(data.list, variable = 'WIDENING_OBSTACLE')
-
-
-state = 'MA'  
-data.list <- getStateDataSets(state, year_selection, year_compare)
-
-view_variable(data.list, variable = 'YEAR_LAST_CONSTRUCTION')
-view_variable(data.list, variable = 'YEAR_LAST_IMPROV')
-
-state = 'WY'  # CO, MN, WY
-
-data.list <- getStateDataSets(state, year_selection, year_compare)
-
-view_variable(data.list, variable = 'SURFACE_TYPE')
-view_variable(data.list, variable = 'FUTURE_AADT')
-view_variable(data.list, var.label = 'Lane Width')
-view_variable(data.list, var.label = 'Through Lanes')
-
-
-state = 'CO'
-data.list <- getStateDataSets(state, year_selection, year_compare)
-
-view_variable(data.list, var.label = 'Urban Code')
-view_variable(data.list, var.label = 'Managed Lanes')
-view_variable(data.list, var.label = 'Number of Signalized Intersections')
-
-# Why is this continuous density -- A: it has 11 unique values
-view_variable(data.list, var.label = 'Lane Width')
-
-# Through lanes - continuous but should be discrete?
-view_variable(data.list, var.label = 'Through Lanes')
-
-view_variable(data.list, variable = gVariables[Label == 'Maintenance & Operations', Name])
