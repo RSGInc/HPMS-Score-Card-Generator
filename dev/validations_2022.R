a68575ce7c38bb1e987d0ce5c3e740af9638bd17
Matt Landis <matt.landis@rsginc.com>
2023-08-31 16:00:46 -0400

    update script that reads validations

--- dev/validations_2022.R
+++ dev/validations_2022.R
@@ -4,41 +4,45 @@ library(data.table)
 
 source("dev/find_project_root.R")
 
-sp_root = find_project_root("HPMS")
+sp_root = find_project_root("HPMS Scorecards")
 fhwa_dir = file.path(sp_root, "2023_update", "from_FHWA")
 
 files = Sys.glob(file.path(fhwa_dir, "Spatial*.csv"))
 data =  lapply(files, fread) 
 names(data) = basename(files)
 
-names(data[[1]])
+names(data[["Spatial Join Full.csv"]])
 
-names(data[[1]]) = c(
+names(data[["Spatial Join Full.csv"]]) = c(
   "rule", "validation_type", "validation_session", "data_item", "data_item_name",
   "message", "update_date", "notes", "update_for_hpms9", "hpms9_change"
 )
 
-names(data[[2]])
+names(data[["Spatial Join Inventory.csv"]])
 
-names(data[[2]]) = c(
+names(data[["Spatial Join Inventory.csv"]]) = c(
   "rule", "validation_session", "validation_type", "data_item", "data_item_name",
   "message", "update_date"
 )
 
-names(data[[3]])
+names(data[["Spatial Join PM2.csv"]])
 
-names(data[[3]]) = c(
+names(data[["Spatial Join PM2.csv"]]) = c(
   "rule", "validation_type", "validation_session", "data_item", "data_item_name",
   "message", "update_date"
 )
 
-names(data[[4]])
+names(data[["Spatial Join Traffic.csv"]])
 
-names(data[[4]]) = c(
+names(data[["Spatial Join Traffic.csv"]]) = c(
   "rule", "validation_session", "validation_type", "data_item", "data_item_name",
   "message", "update_date"
 )
 
 dt = rbindlist(data, use.names = TRUE, fill = TRUE, idcol = "source")
+dt[, .N, .(source, validation_session)]
+dt[, .N, .()]
 
-fwrite(dt, file.path(fhwa_dir, "cross_validation_2022.csv"))
+setkey(dt, validation_type, data_item_name)
+
+fwrite(dt, file.path(fhwa_dir, "validations_2022.csv"))
